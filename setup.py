8b7843f981fd06a801d5faef8c2e3787c4960b65
manishvenu <manish.venumuddula@gmail.com>
2025-10-30 14:00:06 -0600

    Go back

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mom6_bathy", # Replace with your own username
-    version="0.0.2",
+    version="0.0.1",
     author="Alper Altuntas",
     author_email="altuntas@ucar.edu",
     description="MOM6 simple grid and bathymetry generator",
